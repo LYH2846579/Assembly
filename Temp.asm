e3a53b237a035c26b6e1cc25c0a20eed6b7714db
LYH2846579 <1557902391@qq.com>
2021-10-21 15:55:08 +0800

    Hello

--- Temp.asm
+++ Temp.asm
@@ -1,2 +1,17 @@
 assume cs:code,ds:data,ss:stack
 
+data segment
+
+data ends
+
+stack segment stack
+
+stack ends
+
+code segment
+        start:  mov ax,123FH
+
+                mov ax,4C00H
+                int 21H
+code ends
+end start
