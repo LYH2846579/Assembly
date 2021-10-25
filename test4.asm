6550561ab308bbec1fe3d219cbda60a627ae0492
LYH2846579 <1557902391@qq.com>
2021-10-25 21:38:02 +0800

    汇编学习

--- test4.asm
+++ test4.asm
@@ -212,11 +212,74 @@
 
 
 
+;练习6 call指令实现类似switch的结构
+
+;assume cs:code,ds:data,ss:stack             ;汇编编程基本架构
+
+;数据段
+;data segment
+;        dw 100 dup(?)
+;data ends
+
+;堆栈段
+;stack segment stack
+;        dw 15 dup(?)
+;    btm dw ? 
+;stack ends
+                                ;my little house must think it queer, to stop without a far house near.
+;代码段
+;code segment
+;        start:  mov ax,data
+;                mov ds,ax
+;                mov ax,stack
+;                mov ss,ax
+;                mov sp,offset btm
+;                add sp,2
+
+;                mov ax,offset s1        ;将S1地址写入内存
+;                mov ds:[0],ax
+
+;                mov ax,offset s2
+;                mov ds:[2],ax
+
+;                mov ax,offset s3
+;                mov ds:[4],ax
+
+
+                ;选择控制跳转哪一条指令
+;                mov bx,2
+
+;                mov cx,0
+;                jcxz M1002                  ;经验证，跳转指令后面无法直接跟随call指令 -> 所有跳转指令仅仅是修改cs:ip,关执行指令屁事
+;        M1002:  call word ptr ds:[bx]                
+
+;                mov ax,4C00H
+;                int 21H
+
+;           s1:  mov ax,1000H
+;                ret
+;           s2:  mov ax,1002H
+;                ret
+;           s3:  mov ax,1003H
+;                ret     
+;code ends
+;end start
+
+
+
+;练习7 -> 设计一个程序，将以零结尾的字符串输出在屏幕上
+;      -> 接下来在每一行输出完成之后，换行输出
+
+
 assume cs:code,ds:data,ss:stack             ;汇编编程基本架构
 
 ;数据段
-data segment
-        dw 100 dup(?)
+data segment            ;0123456789ABCDEF
+                db      '1) restart pc   ',0
+                db      '2) start system ',0            ;使用jcxz判断
+                db      '3) show clock   ',0
+                db      '4) set clock    ',0
+        crlf:   db      0dh,0ah,'$'   
 data ends
 
 ;堆栈段
@@ -227,39 +290,54 @@ stack ends
                                 ;my little house must think it queer, to stop without a far house near.
 ;代码段
 code segment
-        start:  mov ax,data
-                mov ds,ax
-                mov ax,stack
+        start:  mov ax,stack
                 mov ss,ax
                 mov sp,offset btm
                 add sp,2
 
-                mov ax,offset s1        ;将S1地址写入内存
-                mov ds:[0],ax
+                call init_reg               ;调用初始化寄存器方法
 
-                mov ax,offset s2
-                mov ds:[2],ax
+                mov si,0
+                mov di,160*10 + 30*2        ;屏幕显示赋值区
 
-                mov ax,offset s3
-                mov ds:[4],ax
+                mov cx,4
 
+        begin:  push cx                     ;函数中使用jcxz判断，会将CX值修改，因此先入栈
+                call show_string
+                ;call printCRLF             ;int 21H    中断不适合在动态显示区使用
+                
+                add  si,1                   ;这两行的判断条件分析非常重要!!!   
+                add  di,160-16*2            ;需要根据函数中对si,di的操作及判断条件进行综合分析
+                
+                pop  cx
+                loop begin
 
-                ;选择控制跳转哪一条指令
-                mov bx,2
-
-                mov cx,0
-                jcxz M1002                  ;经验证，跳转指令后面无法直接跟随call指令 -> 所有跳转指令仅仅是修改cs:ip,关执行指令屁事
-        M1002:  call word ptr ds:[bx]                
 
                 mov ax,4C00H
                 int 21H
+;===================================================================
+     init_reg:  mov ax,data     ;1、初始化寄存器
+                mov ds,ax
+                mov ax,0B800H
+                mov es,ax
+                ret
+;===================================================================
+  show_string:  mov cx,0        ;2、显示字符串
+                mov cl,ds:[si]
+                jcxz showStringRet
+                mov es:[di],cl
+                mov es:[di+1],2     ;给输出的字体改个颜色
+                add di,2
+                inc si
+                loop show_string
+;===================================================================
+showStringRet:  ret
+;==================================================================
+    printCRLF:  lea dx,offset crlf
+                mov ah,09H
+                int 21H
+                ret
 
-           s1:  mov ax,1000H
-                ret
-           s2:  mov ax,1002H
-                ret
-           s3:  mov ax,1003H
-                ret     
+
 code ends
 end start
-
